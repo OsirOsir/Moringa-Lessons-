f8f921e42d0686a9bec2dd23ddd94b3a7c2e64bc
Philip osir Otieno <philiposir2@gmail.com>
2024-08-27 10:29:37 +0300

    updates

---
+++ Phase-3-classWork/pythonLessonOne/test.py
@@ -0,0 +1,22 @@
+# my_lastname = "Osir"
+# my_firstname = "Philip"
+# age = 25
+# # print(my_name)
+# # print(my_firstname)
+
+# message = f"My name is {my_firstname} {my_lastname} and my age is {age}"
+
+# print(message)
+
+# event_name = "otamatsuri"
+# activity1 = "cosplay tournament"
+# activity2 = "fifa tornament"
+# activity3 = "lan party"
+# activity4 = "mortal kombat tournament"
+
+# print(activity1, activity2, activity3, activity4)
+
+
+otamatsuri = ['cosplay tournament', 'fifa tournament', 'lan party', 'mortal kombat tornarment']
+
+print(otamatsuri[:])
